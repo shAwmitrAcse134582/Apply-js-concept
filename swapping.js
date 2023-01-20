023225ab35859eae45a950cb10e5d9ddfe2ff7d0
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-01-20 22:45:31 +0600

    swapping

---
+++ swapping.js
@@ -0,0 +1,24 @@
+//swapping two variable using temporary variable
+let a = 10;
+let b = 15;
+// let temp = a;
+// a = b;
+// b = temp;
+// console.log(a, b);
+
+//using destructuring
+// [a, b] = [b, a];
+// console.log(a, b);
+
+//swapping by addition and subtraction
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a, b);
+
+//swapping by xor operation
+a = a ^ b
+b = a ^ b
+a = a ^ b
+console.log(a, b);
